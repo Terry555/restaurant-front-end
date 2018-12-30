a044a78ad3b61567e8e6ccba6ce9223b54b8f2db
github username <github email address>
2018-12-30 15:39:33 -0500

    components are classes now

---
+++ src/Components/Booking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Booking extends Component {
+
+  render(){
+    return();
+  }
+
+}
+
+export default Booking;
---
+++ src/Components/HoursDirections.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HoursDirections extends Component {
+
+  render(){
+    return();
+  }
+
+}
+
+export default HoursDirections;
