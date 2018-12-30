8e0ba6edafccd5b7997ba534c08b1ba5d0f0a2e4
github username <github email address>
2018-12-30 15:35:12 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    products: []
+  }
+
+componentDidMount(){
+  fetch('http://localhost:3000/api/v1/products')
+  .then(response => response.json())
+  .then(data => {
+    this.setState({
+      products: data
+    })
+  })
+}
+
   render() {
     return (
-      <div className="App">
-        TESTING 123
+      <div>
+        <ul>
+          {this.state.products.map(product =>
+            <li key={product.id}>{product.name}, {product.kind}, <img src={product.image_url} alt="food"/></li>
+        )}
+        </ul>
       </div>
     );
   }
